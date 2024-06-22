3312ebf218971b6b6b4697f6ebe9ac3c95f4be4e
LiudmylaBykova <bykova.luda@gmail.com>
2024-06-22 10:50:20 +0300

    add waterModal and waterForm markup and styles

--- src/modals/WaterModal/WaterModal.jsx
+++ src/modals/WaterModal/WaterModal.jsx
@@ -1,9 +1,15 @@
 import WaterForm from "../WaterForm/WaterForm";
+import css from "../WaterModal/WaterModal.module.css";
 
-export default function WaterModal() {
+export default function WaterModal({
+  title = "Add water",
+  subtitle = "Choose a value",
+}) {
   return (
-    <>
+    <div className={css.wrapper}>
+      <h2 className={css.title}>{title}</h2>
+      <p className={css.subtitle}>{subtitle}</p>
       <WaterForm />
-    </>
+    </div>
   );
 }
