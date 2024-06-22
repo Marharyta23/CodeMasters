6f34be8d1415c285570b5cbd31293ea4306339aa
Musteks555 <artemmustek73@gmail.com>
2024-06-23 00:51:54 +0300

    add initial params

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,11 +1,19 @@
+// import { useSelector } from "react-redux";
+
 import UserBarPopover from "../UserBarPopover/UserBarPopover";
 import css from "./UserBar.module.css";
 
-export default function UserBar() {
+export default function UserBar({ user }) {
+    if (!user) {
+        user = "User";
+    }
+
+    // const user = useSelector((state) => state.userName);
+
     return (
         <div className={css.userBarContainer}>
             <button type="button" className={css.userBar}>
-                <p className={css.userBarName}>Nadia</p>
+                <p className={css.userBarName}>{user}</p>
 
                 <img src="" alt="avatar" className={css.userBarImage} width="38" height="38" />
 
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,12 +1,20 @@
+// import { useSelector } from "react-redux";
+
 import UserBar from "../UserBar/UserBar";
 
 import css from "./UserPanel.module.css";
 
-export default function UserPanel() {
+export default function UserPanel({ user }) {
+    if (!user) {
+        user = "User";
+    }
+
+    // const user = useSelector((state) => state.userName);
+
     return (
         <div className={css.container}>
             <p className={css.welcomeText}>
-                Hello<span>, Nadia!</span>
+                Hello<span>, {user}!</span>
             </p>
 
             <UserBar />
