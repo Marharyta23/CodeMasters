aee621209a0b455ace3aee86c763c102278ad633
Musteks555 <artemmustek73@gmail.com>
2024-06-23 21:09:10 +0300

    fix

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -2,9 +2,9 @@ import clsx from "clsx";
 
 import css from "./AddWaterBtn.module.css";
 
-export default function AddWaterBtn({ type }) {
+export default function AddWaterBtn({ type, onClick }) {
     return (
-        <button type="button" className={clsx(css.addWaterBtn, css[type])}>
+        <button type="button" className={clsx(css.addWaterBtn, css[type])} onClick={onClick}>
             {type === "gray" ? (
                 <svg className={css.addWaterBtnIcon} width="16" height="16">
                     <use href="../../../src/img/icons.svg#icon-plus"></use>
--- src/modals/Modal/Modal.jsx
+++ src/modals/Modal/Modal.jsx
@@ -1,44 +1,44 @@
 import Modal from "react-modal";
 
 const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    border: "none",
-    backgroundColor: "rgba(47, 47, 47, 0.60)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    border: "none",
-    backgroundColor: "var(--main-white)",
-    overflow: "auto",
-  },
+    overlay: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        border: "none",
+        backgroundColor: "rgba(47, 47, 47, 0.60)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        border: "none",
+        backgroundColor: "var(--main-white)",
+        overflow: "auto",
+    },
 };
 
 Modal.setAppElement("#root");
 
-export default function ModalWrap({ children, content }) {
-  return (
-    <Modal
-      // isOpen={true} //розкоментовуєте і модалка зʼявиться
-      // onRequestClose={onClose}
-      style={customStyles}
-      shouldCloseOnEsc={true}
-      shouldCloseOnOverlayClick={true}
-      content={content}
-    >
-      {children}
-    </Modal>
-  );
+export default function ModalWrap({ children, content, isOpen, onRequestClose }) {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            style={customStyles}
+            shouldCloseOnEsc={true}
+            shouldCloseOnOverlayClick={true}
+            content={content}
+        >
+            {children}
+        </Modal>
+    );
 }
