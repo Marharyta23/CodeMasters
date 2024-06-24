7882aede9237745fb23051683129ac66b032f232
LiudmylaBykova <bykova.luda@gmail.com>
2024-06-24 19:03:46 +0300

    add addWater and updateWater to redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { popoverReducer } from "./popover/slice";
 import waterReducer from "./deleteWater/deleteWaterSlice";
 import authReducer from "./logout/authSlice";
+import { addWaterReduser } from "./addWater/addWaterSlice";
+import { updateWaterReduser } from "./updateWater/updateWaterSlice";
 
 const store = configureStore({
   reducer: {
     popover: popoverReducer,
     water: waterReducer,
     auth: authReducer,
+    addWater: addWaterReduser,
+    updateWater: updateWaterReduser,
   },
 });
 
