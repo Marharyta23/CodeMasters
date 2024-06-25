bfe70f74fe3ea9df134efecf6bc101dc99ae616a
Musteks555 <artemmustek73@gmail.com>
2024-06-25 11:57:42 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// import { popoverReducer } from "./popover/slice";
-// import waterReducer from "./deleteWater/deleteWaterSlice";
+import { popoverReducer } from "./popover/slice";
+import waterReducer from "./deleteWater/deleteWaterSlice";
+import { authReducer } from "../redux/auth/slice";
+
 // import authReducer from "./logout/authSlice";
 
 // const store = configureStore({
@@ -15,40 +19,29 @@
 // export default store;
 
 /* Viktory' code*/
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
-import { authReducer } from "../redux/auth/slice";
 const authPersistConfig = {
-  key: "auth",
-  storage,
-  whitelist: ["token"],
+    key: "auth",
+    storage,
+    whitelist: ["token"],
 };
 
 const rootReducer = {
-  auth: persistReducer(authPersistConfig, authReducer),
+    auth: persistReducer(authPersistConfig, authReducer),
+    water: waterReducer,
+    popover: popoverReducer,
 };
 
 const middleware = (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  });
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    });
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware,
+    reducer: rootReducer,
+    middleware,
 });
 
 export const persistor = persistStore(store);
