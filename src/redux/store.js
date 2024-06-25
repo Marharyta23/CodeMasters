996f649a6706f2fdf82785d6cd02702ecb230ea3
karolina taran <karolina.taran@gmail.com>
2024-06-25 15:25:39 +0200

    someupdates

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { popoverReducer } from "./popover/slice";
 import waterReducer from "./deleteWater/deleteWaterSlice";
-import authReducer from "./logout/authSlice";
+import authReducerLogout from "./logout/authSlice";
 
 const store = configureStore({
   reducer: {
     popover: popoverReducer,
     water: waterReducer,
-    auth: authReducer,
+    auth: authReducerLogout,
   },
 });
 
