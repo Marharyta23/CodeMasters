f1dcd935cc36c700c703626ca110761f8fc5dfa8
Victoriia Oleh <slavika86@gmail.com>
2024-06-27 22:29:42 +0300

    added Loader and code in auth-slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 import { register, logIn, logOut, refreshUser } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
+    isLoading: false,
     user: {
       name: null,
       email: null,
@@ -15,28 +15,49 @@ const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(register.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
         state.user = payload.data.user;
         state.token = payload.accessToken;
         state.isLoggedIn = true;
       })
+      .addCase(register.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(logIn.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
         state.user = payload.user;
         state.token = payload.accessToken;
         state.isLoggedIn = true;
       })
+      .addCase(logIn.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(logOut.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(logOut.fulfilled, (state) => {
+        state.isLoading = false;
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
+      })
+      .addCase(logOut.rejected, (state) => {
+        state.isLoading = false;
       })
       .addCase(refreshUser.pending, (state) => {
         state.isRefreshing = true;
       })
       .addCase(refreshUser.fulfilled, (state, { payload }) => {
+        state.isRefreshing = false;
         state.user = payload;
         state.isLoggedIn = true;
-        state.isRefreshing = false;
       })
       .addCase(refreshUser.rejected, (state) => {
         state.isRefreshing = false;
