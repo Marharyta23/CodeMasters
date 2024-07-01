af1ed79581f77f1f57a5da33ad62d946f0f9c557
Musteks555 <artemmustek73@gmail.com>
2024-07-01 18:58:41 +0300

    fix loader

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -3,10 +3,7 @@ import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 
 import { selectModalState } from "../../redux/modal/selector";
-import {
-  fetchWaterDataDay,
-  fetchWaterDataMonth,
-} from "../../redux/water/operations";
+import { fetchWaterDataDay, fetchWaterDataMonth } from "../../redux/water/operations";
 
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
@@ -18,47 +15,45 @@ import UserSettingsModal from "../../modals/UserSettingsModal/UserSettingsModal"
 import LogOutModal from "../../modals/LogOutModal/LogOutModal";
 import DeleteWaterModal from "../../modals/DeleteWaterModal/DeleteWaterModal";
 
-import { selectIsLoading } from "../../redux/auth/selectors";
+import { selectWaterLoading } from "../../redux/water/selectors";
 import Loader from "../../components/Loader/Loader";
 
 import css from "./TrackerPage.module.css";
 
 export default function TrackerPage() {
-  const dispatch = useDispatch();
-  const { modalType, props } = useSelector(selectModalState);
+    const dispatch = useDispatch();
+    const { modalType, props } = useSelector(selectModalState);
 
-  const loading = useSelector(selectIsLoading);
+    const loading = useSelector(selectWaterLoading);
 
-  useEffect(() => {
-    const today = new Date();
-    const day = today.getDate();
-    const month = today.getMonth();
-    const year = today.getFullYear();
+    useEffect(() => {
+        const today = new Date();
+        const day = today.getDate();
+        const month = today.getMonth();
+        const year = today.getFullYear();
 
-    dispatch(fetchWaterDataDay({ day, month, year }));
-    dispatch(fetchWaterDataMonth({ month, year }));
-  }, [dispatch]);
+        dispatch(fetchWaterDataDay({ day, month, year }));
+        dispatch(fetchWaterDataMonth({ month, year }));
+    }, [dispatch]);
 
-  return (
-    <>
-      {loading && <Loader />}
-      <DocumentTitle>Water Tracker</DocumentTitle>
+    return (
+        <>
+            {loading && <Loader />}
+            <DocumentTitle>Water Tracker</DocumentTitle>
 
-      <section className={css.tracker}>
-        <WaterMainInfo />
-        <WaterDetailedInfo />
-      </section>
+            <section className={css.tracker}>
+                <WaterMainInfo />
+                <WaterDetailedInfo />
+            </section>
 
-      <ModalWrap modalType={modalType}>
-        {modalType === "WaterModalAdd" && <WaterModal />}
-        {modalType === "WaterModalEdit" && <WaterModal waterRecord={props} />}
-        {modalType === "UserSettingsModal" && <UserSettingsModal />}
-        {modalType === "LogOutModal" && <LogOutModal />}
-        {modalType === "DeleteWaterModal" && (
-          <DeleteWaterModal waterId={props} />
-        )}
-      </ModalWrap>
-      <Toaster />
-    </>
-  );
+            <ModalWrap modalType={modalType}>
+                {modalType === "WaterModalAdd" && <WaterModal />}
+                {modalType === "WaterModalEdit" && <WaterModal waterRecord={props} />}
+                {modalType === "UserSettingsModal" && <UserSettingsModal />}
+                {modalType === "LogOutModal" && <LogOutModal />}
+                {modalType === "DeleteWaterModal" && <DeleteWaterModal waterId={props} />}
+            </ModalWrap>
+            <Toaster />
+        </>
+    );
 }
